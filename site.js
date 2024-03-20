f19a6056a873f605fbe67aaf58f39134ed9922e6
124norellana@frhsd.com <124norellana@frhsd.com>
2024-03-20 09:42:09 -0400

    Steps 1-3

--- site.js
+++ site.js
@@ -33,7 +33,9 @@ const vue_app = Vue.createApp({
         return {
             // This holds your movies.json data.
             movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            title: "IMDB + Michael's Top 8 Movies",
+            owner: "Michael",
+            github: "http://www.github.com/mjslee/is219lee-p3",
          
       }
     },
